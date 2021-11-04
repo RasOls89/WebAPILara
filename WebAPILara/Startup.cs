49b2ee55cd76d782cbea330d6e54703e2fc3b4a2
Rasmus Olsson <rasols8911@gmail.com>
2021-11-04 13:44:08 +0100

    V 0.1

--- WebAPILara/Startup.cs
+++ WebAPILara/Startup.cs
@@ -16,6 +16,7 @@ namespace WebAPILara
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+           services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,11 +29,13 @@ namespace WebAPILara
 
             app.UseRouting();
 
+           app.UseMvc();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("MVC didn't find anything");
                 });
             });
         }
