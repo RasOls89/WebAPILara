a42d3606cedb1eb6eac7efd52ef656da6d84fc9a
Rasmus Olsson <rasols8911@gmail.com>
2021-11-06 13:50:09 +0100

    V 0.5

--- WebAPILara/Startup.cs
+++ WebAPILara/Startup.cs
@@ -7,16 +7,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPILara.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.Extensions.Configuration;
 
 namespace WebAPILara
 {
     public class Startup
     {
+        public IConfiguration Configuration { get;  }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(options => options.EnableEndpointRouting = false); ///Måste lägga till för att kunna köra MVC
+
+            services.AddDbContext<TodoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))); ///Måste installera EF SQL server för att koppla till SQL
 
         }
 
